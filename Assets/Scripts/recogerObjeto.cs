685568789d326b1b71a89ecc463c8bbf55299834
Dante De Badts <dantepetrusdebadts@gmail.com>
2026-05-29 10:27:17 -0300

    puerta, banco, script

---
+++ Assets/Scripts/recogerObjeto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recogerObjeto : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col) {
+        Debug.Log(gameObject.name + " colisionaste con " + col.gameObject.name);
+        if(col.gameObject.CompareTag("paquete")) {
+            Destroy(col.gameObject);
+        }
+    }
+}
