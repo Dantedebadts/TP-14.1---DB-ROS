19f78cbe15f97f280a16feab45c27310d6ef38a5
Dante De Badts <dantepetrusdebadts@gmail.com>
2026-06-03 10:42:35 -0300

    Vidrio paredes y mucho unity

---
+++ Assets/Scripts/TimerC.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerC : MonoBehaviour
+{
+    public TextMeshProUGUI Timer;
+    public float tiempo = 60f;
+    public bool corriendo = true;
+
+    void Start()
+    {
+        int segundos = Mathf.FloorToInt(tiempo);
+        Timer.text = segundos.ToString("00");
+    }
+
+    void Update()
+    {
+        if (corriendo)
+        {
+            if (tiempo > 0)
+            {
+                tiempo -= Time.deltaTime;
+                int segundos = Mathf.FloorToInt(tiempo);
+                Timer.text = segundos.ToString("00");
+            }
+            else
+            {
+                tiempo = 0;
+                corriendo = false;
+                Timer.text = "00";
+            }
+        }
+    }
+}
